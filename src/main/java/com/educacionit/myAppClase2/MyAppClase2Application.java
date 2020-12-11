35777a7a7c7756b04c0a9d40e6f37c6f67cc06e7
Rodolfo Esquerra <rudyesquerra@gmail.com>
2020-12-10 16:34:10 -0800

    primer commit proyecto

---
+++ src/main/java/com/educacionit/myAppClase2/MyAppClase2Application.java
@@ -0,0 +1,13 @@
+package com.educacionit.myAppClase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppClase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppClase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/educacionit/myAppClase2/MyAppClase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacionit.myAppClase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppClase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
